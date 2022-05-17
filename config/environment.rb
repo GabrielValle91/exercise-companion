165b12497c8c7b0062fccaa5fb26c9cf5f3f0e00
GabrielValle91 <gvalle91@gmail.com>
2022-05-17 12:49:50 -0500

    setup mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  address: ENV['MAILGUN_SMTP_SERVER'],
+  port: ENV['MAILGUN_SMTP_PORT'],
+  domain: 'radiant-thicket-85951.herokuapp.com/',
+  user_name: ENV['MAILGUN_SMTP_LOGIN'],
+  password: ENV['MAILGUN_SMTP_PASSWORD'],
+  authenication: :plain,
+  enable_starttls_auto: true
+}
