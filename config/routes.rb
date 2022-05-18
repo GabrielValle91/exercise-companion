4b5d62e42a868eaf69eef631c4b3ba9f55044943
GabrielValle91 <gvalle91@gmail.com>
2022-05-18 13:06:05 -0500

    added way to view all users and their profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
   root 'home#index'
   devise_for :users
+  resources :users, only: [:index, :show]
   get 'home/index'
   get 'about', to: 'home#about'
 
